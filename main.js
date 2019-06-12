08fe1a8543e41d3a57e6652532576517cc97ba5a
Omer Barash <OmerBarash14@gmail.com>
2019-06-12 14:42:51 +0300

    hello

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-const password = "b23"
-const confirmPassword = "bad023"
+const p1 = {
+    name: "robert",
+    age: 22,
+    city: "TLV"
+}
+const p2 = {
+    name: "jill",
+    age: 22 ,
+    city: "TLV"
+}
+if(p1.age = p2.age){
+if(p1.city = p2.city){
+    console.log("jill wanted to date robert")
+    }
+    else{
+        console.log("jill wanted to date robert but could'nt since they are too far away")
+    }
 
-console.log(password == confirmPassword)
+}
+else {
+    console.log("jill and robert dont match ages")
+}
