dd1fb13e77220cf4ee73f97ae98549a16462d6fc
Omer Barash <OmerBarash14@gmail.com>
2019-06-13 10:11:43 +0300

    loops basic exercise 2

--- main.js
+++ main.js
@@ -1,22 +1,8 @@
-const p1 = {
-    name: "robert",
-    age: 22,
-    city: "TLV"
-}
-const p2 = {
-    name: "jill",
-    age: 22 ,
-    city: "TLV"
-}
-if(p1.age = p2.age){
-if(p1.city = p2.city){
-    console.log("jill wanted to date robert")
-    }
-    else{
-        console.log("jill wanted to date robert but could'nt since they are too far away")
-    }
-
-}
-else {
-    console.log("jill and robert dont match ages")
+const one = ['omer' , 'elad' , 'osher']
+const two = [ 21 , 22 , 24]
+let i = 0
+    
+for (let index of one){
+    console.log(index + " is " + two[i] + " years old")
+    i++
 }
